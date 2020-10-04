2ef52b3c7ff66aa4bcc0a43f91ec57a85250070e
Gaurav <gaurav.walia24@gmail.com>
2020-10-04 23:31:55 +0530

    add enzyme test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import nock from 'nock';
+import waitUntil from 'async-wait-until';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({adapter: new Adapter()});
+
+describe('<App />', () => {
+  beforeAll(() => {
+    nock('https://api.spaceXdata.com/v3')
+    .persist()
+    .get('/launches')
+    .reply(200, "all the request");
+  });
+
+  it('Component fetching spacex missions from API', async(done) => {
+    const root = shallow(<App />);
+    let ComponentApp = {};
+    await waitUntil(() => root.state('output').result != null);
+    expect(ComponentApp.result).toEqual(2006);
+    done();
+
+  });
 });
