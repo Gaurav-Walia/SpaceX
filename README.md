1be524165487f0ba5cef5d1dc90388ce5c409014
Gaurav <gaurav.walia24@gmail.com>
2020-10-05 00:34:09 +0530

    added test

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
 
-### `npm start`
+### `npm test`
 
-Runs the tests that run in App.test.js using enzyme
+Runs the tests that run in App.test.js using enzyme and jest
 
 ### `Deployment`
 
--- src/App.test.js
+++ src/App.test.js
@@ -7,20 +7,61 @@ import App from './App';
 
 configure({adapter: new Adapter()});
 
-describe('<App />', () => {
-  beforeAll(() => {
-    nock('https://api.spaceXdata.com/v3')
-    .persist()
-    .get('/launches')
-    .reply(200, "all the request");
-  });
+// describe('<App />', () => {
+//   beforeAll(() => {
+//     nock('https://api.spaceXdata.com/v3')
+//     .persist()
+//     .get('/launches')
+//     .reply(200, "all the request");
+//   });
 
-  it('Component fetching spacex missions from API', async(done) => {
-    const root = shallow(<App />);
-    let ComponentApp = {};
-    await waitUntil(() => root.state('output').result != null);
-    expect(ComponentApp.result).toEqual(2006);
-    done();
+//   it('Component fetching spacex missions from API', async(done) => {
+//     const root = shallow(<App />);
+//     let ComponentApp = {};
+//     await waitUntil(() => root.state('output').result != null);
+//     expect(ComponentApp.result).toEqual(2006);
+//     done();
+
+//   });
+// });
+
+beforeAll(() => {
+  global.fetch = jest.fn();
+});
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<App />, { disableLifecycleMethods: true });
+});
+
+afterEach(() => {
+  wrapper.unmount();
+}); 
+
+const spyDidMount = jest.spyOn(App.prototype, "componentDidMount");
+
+fetch.mockImplementation(() => {
+  return Promise.resolve({
+    status: 200,
+    json: () => {
+      return Promise.resolve({
+        mission_name: "FalconSat"
+      });
+    }
 
   });
+});
+
+const didMount = wrapper.instance().componentDidMount();
+
+expect(spyDidMount).toHaveBeenCalled();
+
+didMount.then(() => {
+  wrapper.update();
+  expect(wrapper.find("p.mission_name").text()).toContain("FalconSat");
+
+  spyDidMount.mockRestore();
+  fetch.mockClear();
+  done();
 });
